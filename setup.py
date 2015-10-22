cd8aaa5659bf0f472ae9d7d2011a5a77f5511dae
Phil Rosenfield <philrosenfield@gmail.com>
2015-10-22 09:49:15 -0400

    modified make_iso_file to include three more columns, X_i on the min L of each TP. Also made small adjustments to diagnostic plots to clean up and speed up

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     name='colibri2trilegal',
     author='Phil Rosenfield',
     author_email='philip.rosenfield@unipd.it',
-    version='2.0',
+    version='2.1',
     url='https://github.com/philrosenfield/colibri2trilegal',
     py_modules=['colibri2trilegal'],
     scripts=['colibri2trilegal'],
