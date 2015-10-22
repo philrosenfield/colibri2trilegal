c66a7552b0299be0d5410be5f05d0f83efc55128
Phil Rosenfield <philrosenfield@gmail.com>
2015-10-22 10:04:12 -0400

    updated contact email

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 setup(
     name='colibri2trilegal',
     author='Phil Rosenfield',
-    author_email='philip.rosenfield@cfa.harvard.ed',
+    author_email='philip.rosenfield@cfa.harvard.edu',
     version='2.1',
     url='https://github.com/philrosenfield/colibri2trilegal',
     py_modules=['colibri2trilegal'],
